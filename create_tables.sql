2ce4caf6f9cde652a463c30609daae799079b380
CaptainCool24 <paarthmhatre08@gmail.com>
2025-07-04 04:38:47 +0530

    commit message

---
+++ create_tables.sql
@@ -0,0 +1,12 @@
+USE demo;
+
+CREATE TABLE students (
+    id    INT PRIMARY KEY AUTO_INCREMENT,
+    name  VARCHAR(100),
+    marks INT
+);
+
+INSERT INTO students (name, marks) VALUES
+    ('Om',     85),
+    ('Suresh', 90),
+    ('Ramesh', 76);
